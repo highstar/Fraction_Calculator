a394c84b3b14ad716670d86fb17bccd8c0aaae4c
高星 <gaoxing@gaos-macbook-pro.local>
2018-06-06 19:08:22 +0800

    Initial Commit

---
+++ Fraction_Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fraction_Calculator
+//
+//  Created by Gao Xing on 2018/6/6.
+//  Copyright © 2018年 Gao Xing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
